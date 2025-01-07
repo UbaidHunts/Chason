53c8e122af7644adc744166b730570c91ac3d90d
Ubaid Bakhtiar <ubaidb@umd.edu>
2025-01-07 15:56:51 -0500

    files

--- src/reset.sh
+++ src/reset.sh
@@ -1,4 +1,4 @@
-#This script file is used to reset the fpga state. 
+# This script file is used to reset the fpga state. 
 # It is optional but recommended. 
 # The previous fpga states can interfere with new dataset output
 xbutil reset -d --force
